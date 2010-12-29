96a873f926f4e8a1320e7c1d5791246dfd427271
Bence Erdos <bence.erdos@proinvento.hu>
2010-12-29 15:25:32 +0100

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BorRedirector::Application.config.secret_token = 'f89412891f5b8743e6adf6990ee92aa3efc9e0d2749fd37ba7d1c2f6f85549911a9fc527a55e062c4328329cf91efc1d861ecae86b46e5b3dff3638c60a4bc08'
