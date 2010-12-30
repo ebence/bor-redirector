091725edfc34aef46e02bc733e20331553c91963
Bence Erdos <bence.erdos@proinvento.hu>
2010-12-30 01:39:35 +0100

    Corrected status in redirect.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,6 @@
 class MainController < ApplicationController
   def redirection
     headers["Status"] = "301 Moved Permanently"
-    redirect_to "http://borozunk.hu"
+    redirect_to "http://borozunk.hu" :status => 301
   end
 end
