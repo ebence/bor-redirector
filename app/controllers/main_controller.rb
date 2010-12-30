6f273c775988ef3e93f2094003a95f672554d851
Bence Erdos <bence.erdos@proinvento.hu>
2010-12-30 02:03:05 +0100

    Redirect status corrected + sqlite added again.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,6 @@
 class MainController < ApplicationController
   def redirection
     headers["Status"] = "301 Moved Permanently"
-    redirect_to "http://borozunk.hu" :status => 301
+    redirect_to "http://borozunk.hu", :status => 301
   end
 end
